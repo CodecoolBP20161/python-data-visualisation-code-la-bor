48b1e705e2fe949350ee38b0ce869f259c0ca008
eszterkaloz <eszterkaloz@gmail.com>
2016-07-12 15:09:38 +0200

    Changed column name: color

--- clients_query.sql
+++ clients_query.sql
@@ -1,3 +1,3 @@
-SELECT company_name, COUNT(company_name), string_agg (main_color, ' ')
+SELECT company_name, COUNT(company_name), string_agg (main_color, ' ') AS color
 FROM project
 GROUP BY company_name;
