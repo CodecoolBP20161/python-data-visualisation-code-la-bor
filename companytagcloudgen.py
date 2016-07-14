d1002e7373eb6d4c50f0eb8c174e2733945aac8d
Áron Kékes <kekes.aron@gmail.com>
2016-07-14 11:18:10 +0200

    added scaling staticmethod

--- companytagcloudgen.py
+++ companytagcloudgen.py
@@ -6,9 +6,16 @@ class CompanyTagCloudGen(TagCloudGen):
 
     def __init__(self, companies):
         super().__init__()
+        min_count = 100
+        max_count = 0
         for company in companies:
-            if company.project_colors:
-                self.tag_cloud_items.append(TagCloudItem(company.name, self.scaling(), self.color_avg(
+            if company.project_count > max_count:
+                max_count = company.project_count
+            elif company.project_count < min_count:
+                min_count = company.project_count
+            elif company.project_colors:
+                self.tag_cloud_items.append(TagCloudItem(company.name, self.scaling(
+                    company, min_count, max_count), self.color_avg(
                     company.project_colors.split(" "))))
 
     @staticmethod
@@ -27,5 +34,18 @@ class CompanyTagCloudGen(TagCloudGen):
         return [color_first_digit, color_second_digit, color_third_digit]
 
     @staticmethod
-    def scaling():
-        return 1
+    def scaling(company, min_count, max_count):
+        scale = max_count - min_count
+        def_font_size = 4
+        if company.project_count > 0.8*scale:
+            return def_font_size * 5
+        elif 0.8*scale > company.project_count > 0.6*scale:
+            return def_font_size * 4
+        elif 0.6*scale > company.project_count > 0.4*scale:
+            return def_font_size * 3
+        elif 0.4*scale > company.project_count > 0.2*scale:
+            return def_font_size * 2
+        elif 0.2*scale > company.project_count > 0:
+            return def_font_size
+        else:
+            raise ValueError("Hülye vagy")
