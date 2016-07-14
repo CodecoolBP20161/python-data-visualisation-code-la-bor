c66073f0390f6bde6f4bbe6eb2aff89bb4c6c17d
lorszil <lorszil@gmail.com>
2016-07-14 18:40:16 +0200

    intresting working

--- display.py
+++ display.py
@@ -12,10 +12,12 @@ class Display:
         img = Image.new("RGB", (700, 700), (255, 255, 255))
         draw = ImageDraw.Draw(img)
         #     # draw.text((x, y),text_content,(r,g,b))
+        z = len(tagcloud) - 100
 
-        for i in range(len(tagcloud)):
-            x = random.randint(0, 500)
-            y = random.randint(0, 600)
+        for i in range(10, z, 30):
+            x += random.randint(-10, 10)
+            y += random.randint(-10, 10)
+
             text_content = tagcloud[i].label
             font_size = int(tagcloud[i].font_size)
             text_size = draw.textsize(text_content)
@@ -26,7 +28,6 @@ class Display:
             draw.text((x, y), text_content, **text_options, font=font)
 
         img.save('image.png')
-
 
     def _check_overlap(self, x, y, font_size, h, w, tagcloud):
         h = tagcloud[i].font_size
