9eebad9c4b85e44669a7aa846f6cc9b8d11336c8
Áron Kékes <kekes.aron@gmail.com>
2016-07-14 14:16:53 +0200

    scaling changed

--- projectcloudgen.py
+++ projectcloudgen.py
@@ -5,13 +5,14 @@ from tagcloudgen import TagCloudItem
 class ProjectTagCloudGen(TagCloudGen):
     def __init__(self, projects):
         super().__init__()
-        min_value = 1000000000000000000
+        min_value = 0
         max_value = 0
         for project in projects:
             if project.value > max_value:
                 max_value = project.value
-            elif project.value < min_value:
+            if project.value < min_value:
                 min_value = project.value
+
         for project in projects:
             if project.value:
                 self.tag_cloud_items.append(TagCloudItem(project.name, self.scaling(
@@ -21,15 +22,26 @@ class ProjectTagCloudGen(TagCloudGen):
     def scaling(project, min_value, max_value):
         scale = max_value - min_value
         def_font_size = 4
-        if project.value > 0.8*scale:
+        # print(scale)
+        if project.value > 0.9 * scale:
+            return def_font_size * 10
+        elif 0.9*scale >= project.value > 0.8 * scale:
+            return def_font_size * 9
+        elif 0.8 * scale >= project.value > 0.7 * scale:
+            return def_font_size * 8
+        elif 0.7 * scale >= project.value > 0.6 * scale:
+            return def_font_size * 7
+        elif 0.6 * scale >= project.value > 0.5 * scale:
+            return def_font_size * 6
+        elif 0.5 * scale >= project.value > 0.4 * scale:
             return def_font_size * 5
-        elif 0.8*scale > project.value > 0.6*scale:
+        elif 0.4 * scale >= project.value > 0.3 * scale:
             return def_font_size * 4
-        elif 0.6*scale > project.value > 0.4*scale:
+        elif 0.3 * scale >= project.value > 0.2 * scale:
             return def_font_size * 3
-        elif 0.4*scale > project.value > 0.2*scale:
+        elif 0.2 * scale >= project.value > 0.1 * scale:
             return def_font_size * 2
-        elif 0.2*scale > project.value > 0:
+        elif 0.1 * scale >= project.value > 0:
             return def_font_size
         else:
             raise ValueError("Hülye vagy")
